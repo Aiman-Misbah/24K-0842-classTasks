3ce995adadbc3d12480fbd98a26598bf197780cf
Aiman Misbah <aimanmisbah06@gmail.com>
2025-09-19 15:38:09 +0500

    Q1 Completed

--- 24K-0842-lab04/Q1.cpp
+++ 24K-0842-lab04/Q1.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+using namespace std;
 
+void bubbleSort(int arr[], int size)
+{
+    int comparisions = 0;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            comparisions++;
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted Elements List: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Element " << (i + 1) << ": " << arr[i] << endl;
+    }
+
+    cout << endl
+         << "Total Number Of Comparisions Performed: " << comparisions << endl;
+}
+
+int main()
+{
+    int arr[] = {12, 34, 56, 23, 76, 43, 51, 55};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << endl;
+    cout << "Original Array Before Sorting: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Element " << (i + 1) << ": " << arr[i] << endl;
+    }
+
+    cout << endl;
+    bubbleSort(arr, size);
+    cout << endl;
+    return 0;
+}
